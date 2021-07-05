b9ec48bce24549c674e4d4c13aaca9d2da4976a0
leiLuo <leiluo@wisejob.cn>
2021-07-05 15:14:39 +0800

    feat:增加utils

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'no-console': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off'
   }
 }
